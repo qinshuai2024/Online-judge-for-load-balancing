2634d86a6f1c92433ff12627c789db511bd1b1ca
qs-liquor <sqin2024@qq.com>
2022-10-26 10:26:21 +0800

    页面渲染模块

--- oj_server/oj_view.hpp
+++ oj_server/oj_view.hpp
@@ -1,1 +1,48 @@
 #pragma once
+
+#include <iostream>
+#include <ctemplate/template.h>
+
+#include "oj_model.hpp"
+
+const std::string template_html = "./template_html/";
+
+namespace ns_view
+{
+    using namespace ns_model;
+    class View
+    {
+    public:
+        void AllExpandHtml(const std::vector<Question>& questions, std::string* html)
+        {
+            std::string src_html = template_html + "all_questions.html";
+            ctemplate::TemplateDictionary root("all_questions");
+
+            for (const auto& q : questions)
+            {
+                ctemplate::TemplateDictionary* sub = root.AddSectionDictionary("question_list");
+                sub->SetValue("number", q.number);
+                sub->SetValue("title", q.title);
+                sub->SetValue("star", q.star);
+            }
+            ctemplate::Template* tpl = ctemplate::Template::GetTemplate(src_html, ctemplate::DO_NOT_STRIP);
+
+            tpl->Expand(html, &root);
+        }
+        void OneExpandHtml(const Question& question, std::string* html)
+        {
+            std::string src_html = template_html + "one_question.html";
+            ctemplate::TemplateDictionary root("one_question");
+            root.SetValue("number", question.number);
+            root.SetValue("title", question.title);
+            root.SetValue("star", question.star);
+            root.SetValue("desc", question.desc);
+            root.SetValue("pre_code", question.header);
+
+            ctemplate::Template* tpl = ctemplate::Template::GetTemplate(src_html, ctemplate::DO_NOT_STRIP);
+
+            tpl->Expand(html, &root);
+        }
+    };
+}
+
